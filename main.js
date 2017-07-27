fe77a1e1ac1440132ee1b6abad0b730e1874e339
Audrey Sperry <audrey.sperry@gmail.com>
2017-07-27 18:11:54 -0400

    background color changes

--- main.js
+++ main.js
@@ -4,6 +4,20 @@
 var intervalId = 0;
 var time = document.querySelector('.clock-time');
 
+
+function convertTimeToHex (h, m, s) {
+  var hoursHex = h.toString(16);
+  var minutesHex = m.toString(16);
+  var secondsHex = s.toString(16);
+console.log(hoursHex, minutesHex, secondsHex);
+  hoursHex = ("O" + hoursHex).slice(-2);
+  minutesHex = ("0" + minutesHex).slice(-2);
+  secondsHex = ("O" + secondsHex).slice(-2);
+  var timeHex = "#" + hoursHex + minutesHex + secondsHex;
+  return timeHex;
+}
+console.log(convertTimeToHex);
+
 function printTimeToScreen () {
   var currentTime = new Date();
   var hours = currentTime.getHours();
@@ -15,15 +29,13 @@ function printTimeToScreen () {
   var seconds = ("0" + seconds).slice(-2);
 
   time.textContent = hours + ":" + minutes + ":" + seconds;
+
+  var timeAsHex = convertTimeToHex(hours, minutes, seconds);
+
+  document.getElementById('body').style.backgroundColor = timeAsHex;
+
 }
 
-function convertTimeToHex (hours, minutes, seconds) {
-  var hoursHex = hours.toString(16);
-  var minutesHex = minutes.toString(16);
-  var secondsHex = seconds.toString(16);
-  var timeHex = "#" + hoursHex + minutesHex + secondsHex;
-  return timeHex; 
-}
 
 
 intervalId = window.setInterval(printTimeToScreen, 1000);
