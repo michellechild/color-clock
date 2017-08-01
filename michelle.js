4b755c75dfd33679b9d585dbc125e8d5fd516206
Michelle Child <michellechild@Michelles-iMac.local>
2017-08-01 19:32:13 -0400

    added progress bar, finished the clock

--- michelle.js
+++ michelle.js
@@ -3,7 +3,36 @@
 
   var timerID = 0;
   var clockTime = document.querySelector('.clockTime');
+  var progressBar = document.querySelector('.progressBar');
   var mouseHover = false;
+
+  //gets random color
+  function randomColorGenerator() {
+    return Math.floor(Math.random()*256);
+  }
+
+  //sets random color as rgb value
+  function randomBackgroundColor() {
+    var red = randomColorGenerator();
+    var green = randomColorGenerator();
+    var blue = randomColorGenerator();
+    var rGB = "rgb(" + red + "," + green + "," +  blue + ")";
+
+    return rGB;
+  }
+
+  //ties random color to the background color
+  function minuteColorChange() {
+      document.body.style.backgroundColor = randomBackgroundColor();
+  }
+
+  function hover() {
+    mouseHover = true;
+  }
+
+  function nohover() {
+    mouseHover = false;
+  }
 
 //displays clock on screen
   function displayTime (hours, minutes, seconds) {
@@ -18,11 +47,17 @@
 
     var time = hours + " : " + minutes + " : " + seconds;
 
+    //sets width of progress bar based on seconds
+    var percentageWidth = ((seconds/60) * 580) + "px";
 
+    //changes color on the second
     if (seconds === "00") {
       minuteColorChange();
     }
+    //displays changing width in progress bar
+    progressBar.style.width = percentageWidth;
 
+    //adds hover effect
     if (mouseHover) {
       clockTime.textContent = document.body.style.backgroundColor;
     } else {
@@ -31,34 +66,13 @@
 
   }
 
-  function randomColorGenerator() {
-    return Math.floor(Math.random()*256);
-  }
-
-  function randomBackgroundColor() {
-    var red = randomColorGenerator();
-    var green = randomColorGenerator();
-    var blue = randomColorGenerator();
-    var rGB = "rgb(" + red + "," + green + "," +  blue + ")";
-
-    return rGB;
-  }
-
-  function minuteColorChange() {
-      document.body.style.backgroundColor = randomBackgroundColor();
-  }
-
-  function hover() {
-    mouseHover = true;
-  }
-
-  function nohover() {
-    mouseHover = false;
-  }
-
   clockTime.addEventListener("mouseover", hover);
   clockTime.addEventListener("mouseout", nohover);
+
+  //changes color on load
   window.onload = minuteColorChange();
+
+  //displays time on load
   window.setInterval(displayTime, 1000);
 
 }());
